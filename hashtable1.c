8f743de68faaa078d58d01acf50b8597e1e3ce9b
samitha <122595628+samitha278@users.noreply.github.com>
2023-01-28 14:39:13 +0530

    Update hashtable1.c
    have bugs, have to improve

--- hashtable1.c
+++ hashtable1.c
@@ -16,20 +16,50 @@ int main(){
     
     //open file
     FILE* myfile;
-    myfile = fopen("//path", "r");
+    myfile = fopen("//path", "rb");
     //check file's avalability
     if(myfile==NULL){
         printf("error occured while opening file");
         return 1;
     }
-    //read file
-    char ch[100];
-    int read_line = fscanf(myfile,"%[^\n]", ch);
-    //check read_line
-    if(read_line==EOF){
-        printf("error occured while reading file");
-        return 1;
+    
+    while((fgetc(myfile))!=EOF){   
+        //read file
+        char ch[100];
+        int read_line = fscanf(myfile,"%[^\n]", ch);
+        //check read_line
+        if(read_line==EOF){
+            printf("error occured while reading file");
+            return 1;
+        }
+
+        //create node for ch in heap
+        node* n = malloc(sizeof(node));
+        //ckeck malloc avalability
+        if(n==NULL){
+            printf("error occured creating node");
+            return 1;
+        }
+        n->name=ch;    //assign string to node->name
+        n->next=NULL;  //assign NULL to next node's address
+
+        //get hash code
+        int key = hash_function(ch);
+
+        //assign NULL to hashtable elements
+        for(int j=0;j<26;j++){
+            hashtable[j]=NULL;
+        }
+
+        //linking node to hashtable
+        if(hashtable[key]==NULL){
+            hashtable[key]=n;
+        }else{
+            n->next=hashtable[key];
+            hashtable[key]=n;
+        }
     }
+    
     //close file
     int ele = fclose(myfile);
     //ckeck ele
@@ -38,30 +68,6 @@ int main(){
         return 1;
     }
     
-    //create node for ch in heap
-    node* n = malloc(sizeof(node));
-    //ckeck malloc avalability
-    if(n==NULL){
-        printf("error occured creating node");
-        return 1;
-    }
-    n->name=ch;    //assign string to node->name
-    n->next=NULL;  //assign NULL to next node's address
-    
-    //get hash code
-    int key = hash_function(ch);
-    
-    //assign NULL to hashtable elements
-    for(int j=0;j<26;j++){
-        hashtable[j]=NULL;
-    }
-    
-    //linking node to hashtable
-    if(hashtable[key]==NULL){
-        hashtable[key]=n;
-    }else
-        n->next=hashtable[key];
-        hashtable[key]=n;
     return 0;
 }
 //create hash function
