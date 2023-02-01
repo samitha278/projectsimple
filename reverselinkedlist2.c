66b760170babef23bb1c90a072705465434cf816
samitha <122595628+samitha278@users.noreply.github.com>
2023-02-01 14:43:51 +0530

    Update reverselinkedlist2.c
    The time complexity of this code is O(n), where n is the number of nodes in the linked list. This is because the code iterates through the linked list twice, once to count the number of nodes and once to reverse the list.
    no syntax errors.

--- reverselinkedlist2.c
+++ reverselinkedlist2.c
@@ -8,7 +8,6 @@ typedef struct node{
 }node;               //create node structure  
 
 node *head=NULL;     //head of linked list 
-int i = 0;
 
 node *createNewNode(int x){
     node *newNode = (node *)malloc(sizeof(node));
@@ -33,34 +32,37 @@ int insertNewNode(int x){
     return 0;
 }                           //insert new node to l.list                
 
-node *getLastNode(){
+
+int nodeCount(){
     if(head==NULL)
-        return NULL;
-    node *last=head;
-    while(last->next!=NULL){
-        last=last->next;
+        return 1;
+    int i=0;
+    node *temp=head;
+    while(temp!=NULL){
+        temp=temp->next;
         i++;
-    }
-    return last; 
-}                           //get last node address
-
-int swap(node *k,node *l){
-    node *temp = k;
-    k=l;
-    l=temp;
-    return 0;
-}
-
+    } 
+    return i;
+}  
 int reverselist(){
     if(head==NULL || head->next==NULL)
         return 1;
-    node *temp=getLastNode();
     
-    node *ptr[i];
-    for(int j=0,n=i/2;j<n;j++){
-        swap(ptr[j],ptr[n-1-j]);
+    node *temp=head;
+    int i = nodeCount();
+    node *(ptr[i]);
+    
+    for(int j=0;j<i;j++){
+        ptr[j]=temp;
+        temp=temp->next;
+        ptr[j]->next=NULL;
     }
-    head=temp;
+    
+    for(int j=0;j<i-1;j++){
+        ptr[i-1-j]->next=ptr[i-2-j];
+    }
+ 
+    head=ptr[i-1];
     return 0;
 }                           //reverse linked list                
 
@@ -72,6 +74,7 @@ int print(){
         printf("%d", temp->data);
         temp=temp->next;
     }
+    printf("\n");
     return 0;
 }                           //print linked list
 
